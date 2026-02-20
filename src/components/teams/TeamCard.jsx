c1b249028f4e337da46f9e24c2b1cec072fcdf2f
mehdibkz82 <mehdibkz82@icloud.com>
2026-02-20 08:47:16 +0000

    File changes

--- src/components/teams/TeamCard.jsx
+++ src/components/teams/TeamCard.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Badge } from "@/components/ui/badge";
-import { Users, Trophy, TrendingUp } from "lucide-react";
+import { Users, Trophy, TrendingUp, Trash2 } from "lucide-react";
 import { useNavigate } from "react-router-dom";
 import { createPageUrl } from "../../utils";
 
